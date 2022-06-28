999de3faa384b7eb25b255b42eaf1234061508f6
THOMAS HARDING <hardingt604@gmail.com>
2022-06-28 08:51:42 -0600

    user signin test

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -4,6 +4,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 // const UserService = require('../lib/services/UserService');
 
+// const ONE_DAY_IN_MS = 1000 * 60 * 60 * 24;
+const IS_DEPLOYED = process.env.NODE_ENV === 'production';
+
 const mockUser = {
   email: 'test@example.com',
   password: '123456',
@@ -26,6 +29,14 @@ describe('users', () => {
       id: expect.any(String),
       email,
     });
+  });
+
+  it('POST /api/v1/users/sessions signs in a new user and creates a cookie', async () => {
+    const res = await request(app).post('/api/v1/users/sessions').send(mockUser);
+    expect(res.body).toEqual({
+      message: 'Signed in successfully!'
+    });
+    expect(res.cookie).toBeTruthy();
   });
 
 });
