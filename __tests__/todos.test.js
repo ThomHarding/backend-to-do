8516819e4d431aeb395212307be63494e74fcf66
THOMAS HARDING <hardingt604@gmail.com>
2022-06-28 09:36:39 -0600

    add test for creating a new todo

--- __tests__/todos.test.js
+++ __tests__/todos.test.js
@@ -1,16 +1,46 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+// const Todo = require('../lib/models/Todo');
 
-describe('items tests', () => {
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '123456',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
+describe('todo tests', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
   afterAll(() => {
     pool.end();
+  });
+  it('POST /api/v1/todos/ creates a new todo for the authenticated user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const newTodo = { task_name: 'test this deliverable' };
+    const resp = await agent.post('/api/v1/todos').send(newTodo);
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      task_name: newTodo.task_name,
+      user_id: user.id,
+      bought: false,
+    });
   });
 });
